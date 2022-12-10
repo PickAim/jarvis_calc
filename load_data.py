ef8538b414aed3578c54793b91a682bce5ff16f5
Pelidyai <dima-pile@mail.ru>
2022-12-10 14:49:28 +0700

    Auto

--- load_data.py
+++ load_data.py
@@ -85,4 +85,5 @@ def load(text: str, update: bool = False, pages_num: int = -1):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         loop.run_until_complete(get_all_product_niche(text, abspath(constants.data_path), pages_num))
+        loop.close()
 
