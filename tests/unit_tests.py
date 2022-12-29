2ef8d10381b32b98bd1fa661ed3cd7558151f788
Pelidyai <dima-pile@mail.ru>
2022-12-30 01:00:21 +0700

    Migrate methods to JDU

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,16 +1,10 @@
-import time
 import unittest
-from os.path import join
 
 import numpy as np
-from utils import constants
 
-from utils.calc_utils import get_frequency_stats, get_nearest_keywords
-from utils.jarvis_utils import load_data
-from utils.load_data import load, get_storage_data
+from utils.calc_utils import get_frequency_stats
 from utils.margin_calc import unit_economy_calc, get_mean_concurrent_cost
 from tests.test_data import cost_data
-from utils.request_utils import  get_object_names, get_parents
 
 
 class MyTestCase(unittest.TestCase):
@@ -19,28 +13,8 @@ class MyTestCase(unittest.TestCase):
         x, y = get_frequency_stats(cost_data, n_samples + 1)
         self.assertEqual(len(x), n_samples + 1)
 
-    def test_load_storage(self):
-        product_ids = [26414401, 6170053]
-        storage_data: dict[int, dict[int, int]] = get_storage_data(product_ids)
-        self.assertIsNotNone(storage_data)
-        self.assertEqual(2, len(storage_data.keys()))
-
-    def test_get_parents(self):
-        parent_categories = get_parents()
-        self.assertNotEqual(len(parent_categories), 0)
-
-    def test_get_objects_name(self):
-        text_object = "Кофе"
-        object_names = get_object_names(text_object)
-        self.assertNotEqual(len(object_names), 0)
-
     def test_unit_economy_calc(self):
-        niche = 'кофе'
-        is_update = False
-        pages_num = 1
-        load(niche, constants.data_path, is_update, pages_num)
-        filename = str(join(constants.data_path, niche + ".txt"))
-        costs = np.array(load_data(filename))
+        costs = np.array(cost_data.copy())
         costs.sort()
         buy = 500
         pack = 150
@@ -49,24 +23,6 @@ class MyTestCase(unittest.TestCase):
         mid_cost = get_mean_concurrent_cost(costs, buy, pack, 20)
         result_dict = unit_economy_calc(buy, pack, mid_cost, transit, unit_count)
         self.assertTrue(result_dict)
-
-    def test_load_n_freq_calc(self):
-        text_to_search = 'молотый кофе'
-        is_update = True
-        pages_num = 1
-        start_time = time.time()
-        load(text_to_search, constants.data_path, is_update, pages_num)
-        print(time.time() - start_time)
-        filename = str(join(constants.data_path, text_to_search + ".txt"))
-        loaded_cost_data = load_data(filename)
-        n_samples = int(len(loaded_cost_data) * 0.1)  # todo think about number of samples
-        x, y = get_frequency_stats(loaded_cost_data, n_samples + 1)
-        self.assertEqual(len(x), n_samples + 1)
-
-    def test_sorting(self):
-        word = "Кофе"
-        result = get_nearest_keywords(word)
-        self.assertEqual("готовый кофе", result[0])
 
 
 if __name__ == '__main__':
--- utils/jarvis_utils.py
+++ utils/jarvis_utils.py
@@ -6,14 +6,3 @@ def create_parser(lst: list[(str, str)]):
     for tup in lst:
         r.add_argument(tup[0], tup[1])
     return r
-
-
-def load_data(filename: str) -> list[float]:
-    result: list[float] = []
-    with (open(filename, "r")) as file:
-        lines: list[str] = file.readlines()
-        for line in lines:
-            for cost in line.split(","):
-                if cost != "" and cost != "\n":
-                    result.append(float(cost))
-    return result
