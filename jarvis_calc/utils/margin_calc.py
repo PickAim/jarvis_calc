13e2877a5b3de067e585cea055fec2f3de1c4f6b
Pelidyai <dima-pile@mail.ru>
2023-01-25 21:06:21 +0700

    Remove old unnecessary methods and tests

--- jarvis_calc/utils/margin_calc.py
+++ jarvis_calc/utils/margin_calc.py
@@ -1,52 +1,8 @@
 from jorm.market.infrastructure import Niche, Warehouse
 from jorm.market.person import Client
-from numpy import ndarray
 
 SAMPLES_COUNT: int = 20
 MONTH: int = 30
-
-
-def unit_economy_calc(buy_price: int,
-                      pack_price: int,
-                      commission: float,
-                      logistic_to_customer: int,
-                      storage_price: int,
-                      returned_percent: float,
-                      client_tax: float,
-                      cost_data: ndarray,
-                      transit_price: int = 0.0,
-                      transit_count: int = 0.0) -> dict:
-    # TODO remove it after DB full implements
-    unit_cost: int = (buy_price + pack_price)
-    mean_concurrent_cost: int = get_mean_concurrent_cost(cost_data, unit_cost, commission,
-                                                         storage_price, logistic_to_customer)
-    result_commission: int = int(mean_concurrent_cost * commission)
-    result_logistic_price: int = logistic_to_customer
-    result_storage_price = storage_price
-    if transit_count > 0:
-        result_logistic_price += transit_price / transit_count
-        result_storage_price = MONTH * storage_price
-    result_product_margin: int \
-        = mean_concurrent_cost - result_commission - result_logistic_price - result_storage_price - unit_cost
-
-    revenue: int = mean_concurrent_cost * transit_count
-    investments: int = unit_cost * transit_count
-    marketplace_expenses: int = int(revenue * commission
-                                    + logistic_to_customer * transit_count
-                                    + transit_count * returned_percent * 33)
-    result_transit_profit: int = revenue - investments - marketplace_expenses - int(revenue * client_tax)
-    return {
-        "Pcost": (buy_price, float(buy_price) / mean_concurrent_cost),  # Закупочная себестоимость
-        "Pack": (pack_price, float(pack_price) / mean_concurrent_cost),  # Упаковка
-        "Mcomm": (result_commission, float(result_commission) / mean_concurrent_cost),  # Комиссия маркетплейса
-        "Log": (result_logistic_price, float(result_logistic_price) / mean_concurrent_cost),  # Логистика
-        "Store": (result_storage_price, float(result_storage_price) / mean_concurrent_cost),  # Хранение
-        "Margin": (result_product_margin, float(result_product_margin) / mean_concurrent_cost),  # Маржа в копейках
-        "Price": (mean_concurrent_cost, float(mean_concurrent_cost) / mean_concurrent_cost),  # Рекомендованная цена
-        "TProfit": (result_transit_profit, 1.0),  # Чистая прибыль с транзита
-        "ROI": (result_transit_profit / investments, 1.0),  # ROI
-        "TMargin": (result_transit_profit / revenue, 1.0)  # Маржа с транзита (%)
-    }
 
 
 def unit_economy_calc_with_jorm(buy_price: int,
@@ -100,33 +56,3 @@ def unit_economy_calc_with_jorm(buy_price: int,
         "ROI": (result_transit_profit / investments, 1.0),  # ROI
         "TMargin": (result_transit_profit / revenue, 1.0)  # Маржа с транзита (%)
     }
-
-
-def get_concurrent_margin(mid_cost: float,
-                          unit_cost: int,
-                          unit_storage_cost: int,
-                          commission: float,
-                          logistic_price: int) -> int:
-    return int(mid_cost - unit_cost - commission * mid_cost - logistic_price - unit_storage_cost)
-
-
-def get_mean_concurrent_cost(cost_data: ndarray,
-                             unit_cost: int,
-                             commission: float,
-                             storage_price: int,
-                             logistic_to_customer: int) -> int:
-    # TODO remove it after DB full implements
-    keys: list[int] = []
-    if len(cost_data) > SAMPLES_COUNT:
-        step: int = len(cost_data) // SAMPLES_COUNT
-        for i in range(SAMPLES_COUNT - 1):
-            keys.append(i * step)
-        keys.append(len(cost_data) - 1)
-    else:
-        keys.extend([0, len(cost_data) - 1])
-    for i in range(1, len(keys)):
-        concurrent_margin: int = get_concurrent_margin(cost_data[keys[i - 1]:keys[i]].mean(), unit_cost,
-                                                       storage_price, commission, logistic_to_customer)
-        if concurrent_margin > 0:
-            return int(cost_data[keys[i - 1]:keys[i]].mean())
-    return int(cost_data[-2:-1].mean())
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -7,7 +7,7 @@ from jorm.market.items import ProductHistory, Product
 from jorm.market.person import Client, ClientInfo
 
 from jarvis_calc.utils.calc_utils import get_frequency_stats, get_frequency_stats_with_jorm
-from jarvis_calc.utils.margin_calc import unit_economy_calc, unit_economy_calc_with_jorm, get_mean_concurrent_cost
+from jarvis_calc.utils.margin_calc import unit_economy_calc_with_jorm
 from jarvis_calc.utils.temporary import get_commission_for
 
 from tests.test_data import cost_data
@@ -15,7 +15,6 @@ from tests.test_data import cost_data
 
 class MyTestCase(unittest.TestCase):
     def test_only_freq_calc(self):
-        # TODO remove it after DB full implements
         n_samples = int(len(cost_data) * 0.1)  # todo think about number of samples
         x, y = get_frequency_stats(cost_data, n_samples)
         self.assertEqual(n_samples, len(x))
@@ -25,16 +24,6 @@ class MyTestCase(unittest.TestCase):
         x, y = get_frequency_stats_with_jorm(niche)
         self.assertEqual(int(len(cost_data) * 0.1), len(x))
 
-    def test_mean_concurrent_cost_calc(self):
-        # TODO remove it after DB full implements
-        costs = np.array(cost_data.copy())
-        costs.sort()
-        buy = 500_00
-        pack = 150_00
-        unit_cost: int = buy + pack
-        mid_cost = get_mean_concurrent_cost(costs, unit_cost, 0.17, 15, 5500)
-        self.assertEqual(971_23, mid_cost)
-
     def test_mean_concurrent_cost_calc_with_niche(self):
         niche: Niche = self.create_test_niche()
         buy = 500_00
@@ -42,23 +31,6 @@ class MyTestCase(unittest.TestCase):
         unit_cost: int = buy + pack
         mid_cost: int = niche.get_mean_concurrent_cost(unit_cost, 5500, 15)
         self.assertEqual(971_23, mid_cost)
-
-    def test_unit_economy_calc(self):
-        # TODO remove it after DB full implements
-        costs = np.array(cost_data.copy())
-        costs.sort()
-        buy: int = 50_00
-        pack: int = 50_00
-        commission: float = 0.17
-        logistic_to_customer: int = 55_00
-        storage: int = 15
-        returned_percent: float = 0.1
-        client_tax: float = 0.06
-        transit_price: int = 1000_00
-        transit_count = 1000
-        result = unit_economy_calc(buy, pack, commission, logistic_to_customer, storage,
-                                   returned_percent, client_tax, costs, transit_price, transit_count)
-        self.assertEqual(71_07, result["Margin"][0])
 
     def test_unit_economy_calc_with_jorm(self):
         niche: Niche = self.create_test_niche()
