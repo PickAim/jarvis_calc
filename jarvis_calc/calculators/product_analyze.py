f9eb3d5557c67fdd3bb518e48af2713f83deea98
Pelidyai <dima-pile@mail.ru>
2023-04-21 23:17:27 +0700

    Add downturn leftovers calculation feature

---
+++ jarvis_calc/calculators/product_analyze.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from jorm.market.items import Product
+
+
+class DownturnCalculator:
+    @staticmethod
+    def calculate(product: Product, from_date: datetime = datetime.utcnow()) -> dict[int, dict[str, int]]:
+        warehouse_id_to_downturn_days: dict[int, dict[str, int]] = {}
+        all_leftovers = product.history.get_all_mapped_leftovers()
+        downturns = product.history.get_leftovers_downturn(from_date)
+        for warehouse_id in downturns:
+            if warehouse_id in all_leftovers:
+                if warehouse_id not in warehouse_id_to_downturn_days:
+                    warehouse_id_to_downturn_days[warehouse_id] = {}
+                for specify in downturns[warehouse_id]:
+                    if specify in all_leftovers[warehouse_id]:
+                        mean_downturn = \
+                            abs(downturns[warehouse_id][specify].sum // downturns[warehouse_id][specify].count)
+                        warehouse_id_to_downturn_days[warehouse_id][specify] = \
+                            all_leftovers[warehouse_id][specify] // mean_downturn if mean_downturn > 0 else -1
+                    else:
+                        warehouse_id_to_downturn_days[warehouse_id][specify] = 0
+        return warehouse_id_to_downturn_days
---
+++ tests/test_product_analyze.py
@@ -0,0 +1,30 @@
+import unittest
+from datetime import datetime
+
+from jorm.market.items import Product
+
+from jarvis_calc.calculators.product_analyze import DownturnCalculator
+from tests.base_test import BaseCalcTest
+
+
+class ProductAnalyzeTest(BaseCalcTest):
+    def test_downturn_calculations(self):
+        product_history = self.create_test_product_history()
+        product = Product("product", 1000, 1, 3.4, product_history)
+        downturn = DownturnCalculator().calculate(product, datetime.utcnow())
+        self.assertEqual({
+            123: {
+                's': 0,
+                'l': 4,
+                'p': -1
+            },
+            321: {
+                's': 0,
+                'l': -1,
+                'p': 6
+            }
+        }, downturn)
+
+
+if __name__ == '__main__':
+    unittest.main()
