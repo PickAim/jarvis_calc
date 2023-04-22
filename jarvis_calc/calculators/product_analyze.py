80440ce61562ca01815e38792c5e73cb922dff4c
Pelidyai <dima-pile@mail.ru>
2023-04-22 16:23:26 +0700

    Remove unnecessary default parameter

--- jarvis_calc/calculators/product_analyze.py
+++ jarvis_calc/calculators/product_analyze.py
@@ -1,11 +1,9 @@
-from datetime import datetime
-
 from jorm.market.items import Product
 
 
 class DownturnCalculator:
     @staticmethod
-    def calculate(product: Product, from_date: datetime = datetime.utcnow()) -> dict[int, dict[str, int]]:
+    def calculate(product: Product, from_date) -> dict[int, dict[str, int]]:
         warehouse_id_to_downturn_days: dict[int, dict[str, int]] = {}
         all_leftovers = product.history.get_all_mapped_leftovers()
         downturns = product.history.get_leftovers_downturn(from_date)
