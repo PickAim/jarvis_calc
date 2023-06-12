3d4600f06b0cb338f335f98995e69ed3c64f0aad
Pelidyai <dima-pile@mail.ru>
2023-06-12 22:33:21 +0700

    Create turnover calculator

--- tests/test_product_analyze.py
+++ tests/test_product_analyze.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from jorm.market.items import Product
 
-from jarvis_calc.calculators.product_analyze import DownturnCalculator
+from jarvis_calc.calculators.product_analyze import DownturnCalculator, TurnoverCalculator
 from tests.base_test import BaseCalcTest
 
 
@@ -24,6 +24,23 @@ class ProductAnalyzeTest(BaseCalcTest):
                 'p': 6
             }
         }, downturn)
+
+    def test_turnover_calculations(self):
+        product_history = self.create_test_product_history()
+        product = Product("product", 1000, 1, 3.4, "brand", "seller", "g", "g", product_history)
+        turnover = TurnoverCalculator().calculate(product, datetime.utcnow())
+        self.assertEqual({
+            123: {
+                's': 15.0,
+                'l': 135.0,
+                'p': 0
+            },
+            321: {
+                's': 15.0,
+                'l': 0,
+                'p': 195.0
+            }
+        }, turnover)
 
 
 if __name__ == '__main__':
