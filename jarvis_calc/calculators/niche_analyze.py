b895974c35c5fd90b045000f4c0d0a8e6322b72e
Pelidyai <dima-pile@mail.ru>
2023-07-19 18:20:01 +0700

    Implement GreenTradeZoneCalculator

--- jarvis_calc/calculators/niche_analyze.py
+++ jarvis_calc/calculators/niche_analyze.py
@@ -1,12 +1,13 @@
+import datetime
 from dataclasses import dataclass
 
 import numpy as np
 from jorm.market.infrastructure import Niche
+from jorm.market.items import Product
 from jorm.support.constants import DAYS_IN_MONTH
 from numpy import ndarray
 
 from jarvis_calc.calculators.calculator_base import Calculator
-from jarvis_calc.utils.calculation_utils import calculate_trade_frequencies, calculate_trade_profits
 
 
 @dataclass
@@ -96,7 +97,7 @@ class NicheCharacteristicsCalculator(Calculator):
 
         top_100_profit = 0
         freq_keys, _ = NicheHistWithNCalculator.calculate(niche.cost_data, 3)
-        trade_profits = calculate_trade_profits(niche.products, freq_keys)
+        trade_profits = NicheCharacteristicsCalculator.__calculate_trade_profits(niche.products, freq_keys)
         max_idx = 0 if len(trade_profits) == 0 else int(np.argmax(trade_profits))
         for product in niche.products:
             product_trade_count = product.history.get_last_month_trade_count()
@@ -117,20 +118,108 @@ class NicheCharacteristicsCalculator(Calculator):
             card_with_trades_count=result_products_with_trades_count,
             daily_mean_niche_profit=int(result_overall_profit / DAYS_IN_MONTH),
             daily_mean_trade_count=int(result_products_trade_count / DAYS_IN_MONTH),
-            mean_traded_card_cost=0 if result_products_trade_count == 0 else int(result_overall_profit / result_products_trade_count),
-            month_mean_niche_profit_per_card=0 if result_card_count == 0 else int(result_overall_profit / result_card_count),
+            mean_traded_card_cost=0 if result_products_trade_count == 0 else int(
+                result_overall_profit / result_products_trade_count),
+            month_mean_niche_profit_per_card=0 if result_card_count == 0 else int(
+                result_overall_profit / result_card_count),
             monopoly_percent=0 if result_overall_profit == 0 else top_100_profit / result_overall_profit,
             maximum_profit_idx=max_idx
         )
 
+    @staticmethod
+    def __calculate_trade_profits(products: list[Product], freq_keys: list[int],
+                                  from_date: datetime.datetime = datetime.datetime.utcnow()) -> list[int]:
+        trade_profits: list[int] = [0 for _ in range(len(freq_keys))]
+        sorted_products = sorted(products, key=lambda prod: prod.cost, reverse=True)
+        j = len(freq_keys) - 1
+        for i, product in enumerate(sorted_products):
+            if j < 0 or j >= len(trade_profits):
+                break
+            if product.cost < freq_keys[j]:
+                j -= 1
+            trade_profits[j] += product.history.get_last_month_trade_count(from_date) * product.cost
+        return trade_profits
+
+
+@dataclass
+class GreenTradeZoneCalculateResult:
+    segments: list[tuple[int, int]]
+    best_segment_idx: int
+
+    segment_profits: list[int]
+    best_segment_profit_idx: int
+
+    mean_segment_profit: list[int]
+    best_mean_segment_profit_idx: int
+
+    mean_product_profit: list[int]
+    best_mean_product_profit_idx: int
+
+    segment_product_count: list[int]
+    best_segment_product_count_idx: int
+
+    segment_product_with_trades_count: list[int]
+    best_segment_product_with_trades_count_idx: int
+
 
 class GreenTradeZoneCalculator(Calculator):
     @staticmethod
-    def calculate(niche: Niche):
-        freq_keys, frequencies = NicheHistCalculator.calculate(niche)
-        products = niche.products
-        trade_frequencies = calculate_trade_frequencies(products, freq_keys)
+    def calculate(niche: Niche,
+                  from_date: datetime.datetime = datetime.datetime.utcnow()) -> GreenTradeZoneCalculateResult:
+        res = np.histogram(niche.cost_data, 10)
+        freq_keys, frequencies = list(map(int, res[1][1:])), list(res[0])
+        sorted_products = sorted(niche.products, key=lambda prod: prod.cost, reverse=True)
 
-        # plot.plot(freq_keys, trade_frequencies)
-        # plot.plot(freq_keys, frequencies)
-        # plot.show()
+        segment_profits: list[int] = [0 for _ in range(len(freq_keys))]
+        product_with_profit_counts: list[int] = [0 for _ in range(len(freq_keys))]
+        j = len(freq_keys) - 1
+        for i, product in enumerate(sorted_products):
+            if j < 0 or j >= len(segment_profits):
+                break
+            if product.cost < freq_keys[j]:
+                j -= 1
+            product_profit = int(product.history.get_last_month_trade_count(from_date) * product.cost)
+            if product_profit > 0:
+                product_with_profit_counts[j] += 1
+                segment_profits[j] += product_profit
+
+        segments: list[tuple[int, int]] = [(freq_keys[i], freq_keys[i + 1]) for i in range(len(freq_keys) - 1)]
+        segments.append((freq_keys[len(freq_keys) - 1], sorted_products[0].cost))
+
+        mean_segment_profit: list[int] = [
+            segment_profits[i] // frequencies[i] if frequencies[i] != 0 else 0
+            for i in range(len(segment_profits))
+        ]
+        mean_product_profit: list[int] = [
+            segment_profits[i] // product_with_profit_counts[i] if product_with_profit_counts[i] != 0 else 0
+            for i in range(len(segment_profits))
+        ]
+        best_segment_profit_idx: int = int(np.argmax(segment_profits))
+        best_mean_segment_profit_idx: int = int(np.argmax(mean_segment_profit))
+        best_mean_product_profit_idx: int = int(np.argmax(mean_product_profit))
+        best_segment_product_count_idx: int = int(np.argmin(frequencies))
+        best_segment_product_with_trades_count_idx: int = int(np.argmax(product_with_profit_counts))
+
+        counter = {}
+        for integer in [best_segment_profit_idx, best_mean_segment_profit_idx,
+                        best_mean_product_profit_idx, best_segment_product_count_idx,
+                        best_segment_product_with_trades_count_idx]:
+            if integer not in counter:
+                counter[integer] = 0
+            counter[integer] += 1
+
+        best_segment_idx: int = max(counter, key=counter.get)
+        return GreenTradeZoneCalculateResult(
+            segments=segments,
+            best_segment_idx=best_segment_idx,
+            segment_profits=segment_profits,
+            best_segment_profit_idx=best_segment_profit_idx,
+            mean_segment_profit=mean_segment_profit,
+            best_mean_segment_profit_idx=best_mean_segment_profit_idx,
+            mean_product_profit=mean_product_profit,
+            best_mean_product_profit_idx=best_mean_product_profit_idx,
+            segment_product_count=frequencies,
+            best_segment_product_count_idx=best_segment_product_count_idx,
+            segment_product_with_trades_count=product_with_profit_counts,
+            best_segment_product_with_trades_count_idx=best_segment_product_with_trades_count_idx
+        )
--- tests/test_niche_analyze.py
+++ tests/test_niche_analyze.py
@@ -1,9 +1,10 @@
 import unittest
 
+import matplotlib.pyplot as plot
 from jorm.market.infrastructure import Niche
 
 from jarvis_calc.calculators.niche_analyze import NicheCharacteristicsCalculator, NicheHistCalculator, \
-    NicheCharacteristicsCalculateResult, GreenTradeZoneCalculator
+    NicheCharacteristicsCalculateResult, GreenTradeZoneCalculator, GreenTradeZoneCalculateResult
 from tests.base_test import BaseCalcTest
 from tests.data_for_tests import cost_data
 
@@ -40,7 +41,78 @@ class NicheAnalyzeTest(BaseCalcTest):
 
     def test_green_zone(self):
         niche: Niche = self.create_test_niche()
-        GreenTradeZoneCalculator.calculate(niche)
+        result = GreenTradeZoneCalculator.calculate(niche)
+        # self.draw_green_zone_result(result) # uncomment if you need draw results
+        self.assertEqual([(263550, 523800), (523800, 784050), (784050, 1044300),
+                          (1044300, 1304550), (1304550, 1564800), (1564800, 1825050),
+                          (1825050, 2085300), (2085300, 2345550), (2345550, 2605800), (2605800, 2605800.0)],
+                         result.segments)
+        self.assertEqual([224035864, 40938752, 79150750, 0, 0, 0, 4103700, 0, 3945400, 5499900], result.segment_profits)
+        self.assertEqual([59034, 162455, 1978768, 0, 0, 0, 0, 0, 0, 5499900], result.mean_segment_profit)
+        self.assertEqual([1851536, 3411562, 4655926, 0, 0, 0, 4103700, 0, 3945400, 2749950], result.mean_product_profit)
+        self.assertEqual([3795, 252, 40, 19, 4, 1, 0, 2, 0, 1], result.segment_product_count)
+        self.assertEqual([121, 12, 17, 0, 0, 0, 1, 0, 1, 2], result.segment_product_with_trades_count)
+
+    def draw_green_zone_result(self, result: GreenTradeZoneCalculateResult):
+        keys_to_draw = [key[0] // 100 for key in result.segments]
+        segment_profits_to_draw = [key // 100_000 for key in result.segment_profits]
+        mean_segment_profits_to_draw = [key // 100 for key in result.mean_segment_profit]
+        mean_segment_product_profit_to_draw = [key // 100 for key in result.mean_product_profit]
+        segment_product_count_to_draw = [key for key in result.segment_product_count]
+        segment_product_with_trades_count_to_draw = [key for key in result.segment_product_with_trades_count]
+
+        figure = plot.figure()
+
+        figure.add_subplot(2, 3, 3)
+        plot.title("Segment profits")
+        self.draw_zone(result.best_segment_profit_idx, keys_to_draw)
+        plot.plot(keys_to_draw, segment_profits_to_draw)
+        plot.grid()
+
+        figure.add_subplot(2, 3, 1)
+        plot.title("Mean segment profits")
+        self.draw_zone(result.best_mean_segment_profit_idx, keys_to_draw)
+        plot.plot(keys_to_draw, mean_segment_profits_to_draw)
+        plot.grid()
+
+        figure.add_subplot(2, 3, 2)
+        plot.title("Mean product profit")
+        self.draw_zone(result.best_mean_product_profit_idx, keys_to_draw)
+        plot.plot(keys_to_draw, mean_segment_product_profit_to_draw)
+        plot.grid()
+
+        figure.add_subplot(2, 3, 5)
+        plot.title("Product counts")
+        self.draw_zone(result.best_segment_product_count_idx, keys_to_draw)
+        plot.plot(keys_to_draw, segment_product_count_to_draw)
+        plot.grid()
+
+        figure.add_subplot(2, 3, 4)
+        plot.title("Products with trade counts")
+        self.draw_zone(result.best_segment_product_with_trades_count_idx, keys_to_draw)
+        plot.plot(keys_to_draw, segment_product_with_trades_count_to_draw)
+        plot.grid()
+
+        subplot = figure.add_subplot(2, 3, 6)
+        plot.title("Result recommendation")
+        grid_ticks = [keys_to_draw[i] for i in range(0, len(keys_to_draw), 2)]
+        grid_ticks.append(keys_to_draw[len(keys_to_draw) - 1] + keys_to_draw[0])
+        grid_ticks.append(keys_to_draw[result.best_segment_idx])
+        grid_ticks.append(keys_to_draw[result.best_segment_idx + 1])
+        subplot.set_xticks(grid_ticks)
+        plot.axvline(x=keys_to_draw[result.best_segment_idx], color='lime', label=f'best zone')
+        plot.axvline(x=keys_to_draw[result.best_segment_idx + 1], color='lime', label=f'best zone')
+        plot.grid()
+
+        plot.show()
+
+    @staticmethod
+    def draw_zone(idx: int, keys_to_draw: list[int]):
+        for i in range(len(keys_to_draw)):
+            if i == idx or i == idx + 1:
+                plot.axvline(x=keys_to_draw[i], color='lime', label=f'zone {i}')
+            else:
+                plot.axvline(x=keys_to_draw[i], color='r', label=f'zone {i}')
 
 
 if __name__ == '__main__':
