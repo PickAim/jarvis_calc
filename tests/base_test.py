5b16cfcac9cb678673e80ca717ad1929c3f48a7f
Pelidyai <dima-pile@mail.ru>
2023-11-25 23:17:06 +0700

    JORM deps update. Adapt ot JORM changes. Raise version (v0.1.28)

--- tests/base_test.py
+++ tests/base_test.py
@@ -38,7 +38,8 @@ class BaseCalcTest(unittest.TestCase):
             spec_leftovers: list[SpecifiedLeftover] = [SpecifiedLeftover("second", self.leftover_func(cost))]
             before_trade_storage_dict = StorageDict()
             before_trade_storage_dict[1] = spec_leftovers
-            products.append(Product(f'prod{i}', cost, i, 4.0, "brand", "seller", niche_name, "default_category",
+            products.append(Product(f'prod{i}', cost, i, 4.0, "brand", "seller",
+                                    category_niche_list=[("default_category", niche_name)],
                                     history=ProductHistory([
                                         ProductHistoryUnit(1, datetime.utcnow(), before_trade_storage_dict),
                                         ProductHistoryUnit(3, datetime.utcnow(), StorageDict())]),
